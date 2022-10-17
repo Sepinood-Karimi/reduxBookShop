467d72a5fd2e06e514bf56670d63c87bca6e31cd
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-10-17 09:05:43 +0330

    remove from cart state and logic

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,28 +1,39 @@
 import classes from './CartItem.module.css';
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../store/cart-slice";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+    const {id, title, quantity, total, price} = props.item;
 
-  return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
-  );
+    const dispatch = useDispatch();
+
+    const addItemToCartHandler = () => {
+        dispatch(cartActions.addItemToCart({id,title, quantity, totalPrice: total, price}));
+    };
+    const removeItemFromCartHandler = () => {
+        dispatch(cartActions.removeItemFromCart(id));
+    }
+
+    return (
+        <li className={classes.item}>
+            <header>
+                <h3>{title}</h3>
+                <div className={classes.price}>
+                    ${total.toFixed(2)}{' '}
+                    <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+                </div>
+            </header>
+            <div className={classes.details}>
+                <div className={classes.quantity}>
+                    x <span>{quantity}</span>
+                </div>
+                <div className={classes.actions}>
+                    <button onClick={removeItemFromCartHandler}>-</button>
+                    <button onClick={addItemToCartHandler}>+</button>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default CartItem;
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,29 +1,39 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const cartInitialState = {
-  items : [],
-  totalQuantity : 0
+    items: [],
+    totalQuantity: 0
 };
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: cartInitialState,
-    reducers : {
-        addItemToCart(state,action){
+    reducers: {
+        addItemToCart(state, action) {
             const newItem = action.payload;
-            const existingItem = state.items.find(item=>item.id===newItem.id);
-            state.totalQuantity ++;
-            if (existingItem){
+            const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
+            if (existingItem) {
                 existingItem.quantity++;
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-            }else{
+            } else {
                 state.items.push({
-                    id : newItem.id,
-                    title : newItem.title,
-                    price : newItem.price,
-                    quantity : 1,
-                    totalPrice : newItem.price
+                    id: newItem.id,
+                    title: newItem.title,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price
                 })
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id === id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
         }
     }
